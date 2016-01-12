0af4955ff49c52e726d860e21a3823b2118a1474
Sean <toranos@gmail.com>
2016-01-12 20:29:46 +1100

    Add user details

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.route('/bleat/:id')
         });
     });
 
+app.route('/user/:username/details')
+    .get(function(request, response) {
+        var username = request.params.username;
+        var path = __dirname + '\\dataset-medium\\users\\' + username + '\\details.txt';
+        fs.readFile(path, function(err, data) {
+            if (err) {
+                response.status(404).json('No user with that username');
+                return;
+            }
+            data = data.toString();
+            data = data.replace(/([^:]+): (.+)\n/g, function(a, b, c) {
+                return '"'+b+'": ' + '"'+c+'",';
+            })
+            data = data.slice(0, -1);
+            data = '{' + data + '}';
+            data = JSON.parse(data);
+            response.json(data);
+        });
+    });
+
 app.listen(8080);
