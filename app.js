8d4388e99337a390043d47c8290dd12afd3f46c9
Sean <toranos@gmail.com>
2016-01-19 18:05:26 +1100

    Use getBleat for normal bleat route

--- app.js
+++ app.js
@@ -7,19 +7,7 @@ app.use(express.static('public'));
 app.route('/bleat/:id')
     .get(function(request, response) {
         var id = request.params.id;
-        var path = __dirname + '\\dataset-medium\\bleats\\' + id;
-        fs.readFile(path, function(err, data) {
-            if (err) {
-                response.status(404).json('No bleat with that id');
-                return;
-            }
-            data = data.toString();
-            data = data.replace(/([^:]+): (.+)\n/g, function(a, b, c) {
-                return '"'+b+'": ' + '"'+c+'",';
-            })
-            data = data.slice(0, -1);
-            data = '{' + data + '}';
-            data = JSON.parse(data);
+        getBleat(id, function(data) {
             response.json(data);
         });
     });
