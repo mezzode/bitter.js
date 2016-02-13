8b2f9ce375de137c3fa275d49379ef1b57b86191
Sean <toranos@gmail.com>
2016-02-13 19:06:20 +1100

    Clean up populate.py

--- populate.py
+++ populate.py
@@ -1,9 +1,7 @@
 #!python
-
 import os, sys, sqlite3, bcrypt
 
 users_dir = './dataset-medium/users'
-
 conn = sqlite3.connect('bitter.db')
 c = conn.cursor()
 
@@ -27,28 +25,9 @@ for user in os.listdir(users_dir):
             keys.append(field)
             values.append(value)
     insert_str = "INSERT INTO users (%s) values (%s);" % (",".join(keys),",".join(['?']*len(keys)))
-    # print insert_str, ','.join(values)
     c.execute(insert_str, values)
 
     for listen in listens:
         c.execute("INSERT INTO listens (username, listen) values (?,?);", (user, listen))
 
 conn.commit()
-
-# for user in os.listdir(users_dir):
-#     details = {}
-#     with open(os.path.join(users_dir,user,"details.txt")) as f:
-#         for line in f:
-#             field, _, value = line.rstrip().partition(": ")
-#             details[field] = value
-#     values = []
-#     for key in keys:
-#         if key in details:
-#             values.append(details[key])
-#         else:
-#             values.append(None)
-#     insert_str = "INSERT INTO users (%s) values (%s)" % (",".join(keys),",".join(['?']*len(keys)))
-#     c.execute(insert_str, values)
-# with open(os.path.join(users_dir,user,"bleats.txt")) as f:
-#     self.bleats = f.read().split()
-#     self.bleats.sort(reverse=True)
