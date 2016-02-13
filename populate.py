04d0dcacb42cc7d1c80a02dfb04658c2126afa4c
Sean <toranos@gmail.com>
2016-02-13 19:26:59 +1100

    Add bleats to database

--- populate.py
+++ populate.py
@@ -2,6 +2,7 @@
 import os, sys, sqlite3, bcrypt
 
 users_dir = './dataset-medium/users'
+bleats_dir = './dataset-medium/bleats'
 conn = sqlite3.connect('bitter.db')
 c = conn.cursor()
 
@@ -29,5 +30,21 @@ for user in os.listdir(users_dir):
 
     for listen in listens:
         c.execute("INSERT INTO listens (username, listen) values (?,?);", (user, listen))
+
+all_keys = ['username', 'bleat', 'time', 'latitude', 'longitude', 'in_reply_to']
+c.execute("CREATE TABLE bleats (%s,id PRIMARY KEY);" % ",".join(all_keys))
+for bleat in os.listdir(bleats_dir):
+    keys = []
+    values = []
+    listens = []
+    with open(os.path.join(bleats_dir,bleat)) as f:
+        for line in f:
+            field, _, value = line.rstrip().partition(": ")
+            keys.append(field)
+            values.append(value)
+        keys.append('id')
+        values.append(bleat)
+    insert_str = "INSERT INTO bleats (%s) values (%s);" % (",".join(keys),",".join(['?']*len(keys)))
+    c.execute(insert_str, values)
 
 conn.commit()
