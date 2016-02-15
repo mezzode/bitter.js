0e01086c526c9f788dcf25d945da83389d9976f0
Sean <toranos@gmail.com>
2016-02-15 21:03:05 +1100

    Change login button to depend on field values

--- public/login.jsx
+++ public/login.jsx
@@ -3,15 +3,10 @@ import React from 'react';
 export default class Login extends React.Component {
     constructor() {
         super();
-        this.state = {disabled: 'disabled', username: ''};
+        this.state = {disabled: 'disabled', username: '', password: ''};
     }
     check(event) {
-        this.setState({username: event.target.value});
-        if (event.target.value.length > 0) {
-            this.setState({disabled: false});
-        } else {
-            this.setState({disabled: 'disabled'});
-        }
+        this.setState({[event.target.name]: event.target.value});
     }
     render() {
         return (
@@ -25,17 +20,17 @@ export default class Login extends React.Component {
                         <div className="modal-body">
                             <form method="POST" action="/api/authenticate" login>
                             <div className="form-group">
-                            <input type="text" className="form-control" name="user" placeholder="Username" value={this.state.username} onChange={this.check.bind(this)}/>
+                            <input type="text" className="form-control" name="username" placeholder="Username" value={this.state.username} onChange={this.check.bind(this)}/>
                             </div>
                             <div className="form-group">
-                            <input type="password" className="form-control" name="pass" placeholder="Password"/>
+                            <input type="password" className="form-control" name="password" placeholder="Password" value={this.state.password} onChange={this.check.bind(this)}/>
                             </div>
                             <div className="checkbox">
                                 <label>
                                     <input type="checkbox" name="remember-me"/> Remember Me
                                  </label>
                             </div>
-                            <button type="submit" className="btn btn-default" disabled={this.state.disabled}>Submit</button>
+                            <button type="submit" className="btn btn-default" disabled={this.state.username && this.state.password ? false : 'disabled'}>Submit</button>
                             
                             </form>
                         </div>
