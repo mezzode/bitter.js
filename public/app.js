79be180f318c3d2f41a74c15a430b94b9a19da6d
Sean <toranos@gmail.com>
2016-02-19 22:24:17 +1100

    Add landing page

--- public/app.js
+++ public/app.js
@@ -62,7 +62,14 @@ class Home extends React.Component {
     render() {
         return (
             <div className="row">
-                Home
+                <div className="col-md-12">
+                    <div className="jumbotron">
+                        <h1>Welcome to Bitter</h1>
+                        <p>The latest and greatest thing since sliced bread!</p>
+                        <br/>
+                        <p><Link className="btn btn-primary btn-lg" to='/signup' role="button">Join Now</Link></p>
+                    </div>
+                </div>
             </div>
         );
     }
