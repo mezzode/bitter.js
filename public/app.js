b3384be6053c2961c968defcea5d26205b6e570b
Sean <toranos@gmail.com>
2016-02-20 12:01:19 +1100

    Add signup page

--- public/app.js
+++ public/app.js
@@ -66,8 +66,51 @@ class Settings extends React.Component {
 }
 
 class Signup extends React.Component {
+    submit(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log('Submitted');
+    }
     render() {
-        return(<div>Moo</div>);
+        return(
+            <div className="row">
+                <div className="col-md-3">
+                </div>
+                <div className="col-md-6 col-sm-12">
+                    <h1>New Profile</h1>
+                    <form method="POST" id="new-user-required" onSubmit={this.submit}>
+                        <input name="new-validate" value="True" type="hidden"/>
+                        <div className="form-group">
+                            <label>Full Name</label>
+                            <input name="full-name" value="" className="form-control" placeholder="Full Name" type="text"/>
+                            <span id="name-help" className="help-block"></span>
+                        </div>
+                        <div className="form-group">
+                            <label>Email Address</label>
+                            <input name="email" value="" className="form-control" placeholder="Email" type="email"/>
+                            <span id="email-help" className="help-block"></span>
+                        </div>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input name="new-username" value="" className="form-control" placeholder="Username" type="text"/>
+                            <span id="username-help" className="help-block"></span>
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="new-password" value="" className="form-control" placeholder="Password" type="password"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Confirm Password</label>
+                            <input name="new-password-confirm" value="" className="form-control" placeholder="Confirm Password" type="password"/>
+                            <span id="password-help" className="help-block"></span>
+                        </div>
+                        <button type="submit" name="new-user" value="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div className="col-md-3">
+                </div>
+            </div>
+        );
     }
 }
 
