3a711a9cf5e37c9fe25fa6c432e6edf500372a42
Sean <toranos@gmail.com>
2016-02-21 13:52:51 +1100

    Add onChange handler to signup

--- public/signup.jsx
+++ public/signup.jsx
@@ -16,6 +16,9 @@ export default class Signup extends React.Component {
         e.stopPropagation();
         console.log('Submitted');
     }
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
     render() {
         const {name, email, username, password, confirm} = this.state;
         return(
@@ -25,11 +28,11 @@ export default class Signup extends React.Component {
                 <div className="col-md-6 col-sm-12">
                     <h1>New Profile</h1>
                     <form onSubmit={this.submit}>
-                        <Input name="name" title="Full Name" type="text" value={name}/>
-                        <Input name="email" title="Email" type="email" value={email}/>
-                        <Input name="username" title="Username" type="text" value={username}/>
-                        <Input name="password" title="Password" type="password" value={password}/>
-                        <Input className="form-control" title="Confirm Password" type="password" value={confirm}/>
+                        <Input name="name" title="Full Name" type="text" value={name} onChange={this.onChange.bind(this)} onChange={this.onChange.bind(this)}/>
+                        <Input name="email" title="Email" type="email" value={email} onChange={this.onChange.bind(this)}/>
+                        <Input name="username" title="Username" type="text" value={username} onChange={this.onChange.bind(this)}/>
+                        <Input name="password" title="Password" type="password" value={password} onChange={this.onChange.bind(this)}/>
+                        <Input className="form-control" title="Confirm Password" type="password" value={confirm} onChange={this.onChange.bind(this)}/>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                 </div>
@@ -42,11 +45,11 @@ export default class Signup extends React.Component {
 
 class Input extends React.Component {
     render() {
-        const {name, value, title, type, error} = this.props;
+        const {name, value, title, type, error, onChange} = this.props;
         return (
             <div className="form-group">
                 <label>{title}</label>
-                <input name={name} className="form-control" placeholder={title} type={type} value={value}/>
+                <input name={name} className="form-control" placeholder={title} type={type} value={value} onChange={onChange}/>
                 <span className="help-block">{error}</span>
             </div>
         );
