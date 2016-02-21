0cddadd5e17e86e1239a76bf3b7cbac62aa1b733
Sean <toranos@gmail.com>
2016-02-21 13:35:20 +1100

    Begin changing signup to react

--- public/app.js
+++ public/app.js
@@ -66,45 +66,55 @@ class Settings extends React.Component {
 }
 
 class Signup extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            username: '',
+            password: '',
+            confirm: ''
+        };
+    }
     submit(e) {
         e.preventDefault();
         e.stopPropagation();
         console.log('Submitted');
     }
     render() {
+        const {name, email, username, password, confirm} = this.state;
         return(
             <div className="row">
                 <div className="col-md-3">
                 </div>
                 <div className="col-md-6 col-sm-12">
                     <h1>New Profile</h1>
-                    <form method="POST" id="new-user-required" onSubmit={this.submit}>
-                        <input name="new-validate" value="True" type="hidden"/>
+                    <form onSubmit={this.submit}>
                         <div className="form-group">
                             <label>Full Name</label>
-                            <input name="full-name" value="" className="form-control" placeholder="Full Name" type="text"/>
-                            <span id="name-help" className="help-block"></span>
+                            <input name="name" className="form-control" placeholder="Full Name" type="text" value={name}/>
+                            <span className="help-block"></span>
                         </div>
                         <div className="form-group">
                             <label>Email Address</label>
-                            <input name="email" value="" className="form-control" placeholder="Email" type="email"/>
-                            <span id="email-help" className="help-block"></span>
+                            <input name="email" className="form-control" placeholder="Email" type="email" value={email}/>
+                            <span className="help-block"></span>
                         </div>
                         <div className="form-group">
                             <label>Username</label>
-                            <input name="new-username" value="" className="form-control" placeholder="Username" type="text"/>
-                            <span id="username-help" className="help-block"></span>
+                            <input name="username" className="form-control" placeholder="Username" type="text" value={username}/>
+                            <span className="help-block"></span>
                         </div>
                         <div className="form-group">
                             <label>Password</label>
-                            <input name="new-password" value="" className="form-control" placeholder="Password" type="password"/>
+                            <input name="password" className="form-control" placeholder="Password" type="password" value={password}/>
                         </div>
                         <div className="form-group">
                             <label>Confirm Password</label>
-                            <input name="new-password-confirm" value="" className="form-control" placeholder="Confirm Password" type="password"/>
-                            <span id="password-help" className="help-block"></span>
+                            <input className="form-control" placeholder="Confirm Password" type="password" value={confirm}/>
+                            <span className="help-block"></span>
                         </div>
-                        <button type="submit" name="new-user" value="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                 </div>
                 <div className="col-md-3">
