5e4e3d482b2f40512ee826f2333980c940a0e999
Sean <toranos@gmail.com>
2016-02-23 17:50:49 +1100

    Change placeholder

--- app.js
+++ app.js
@@ -224,12 +224,10 @@
                     });
                 });
             } else {
-                // send confirmation email, blah
-                // encode token with user details into url in email?
                 jwt.sign({username: username, name: name, email: email, password: password}, 'secret', {expiresIn: "30d"}, function(token) {
                     var url = 'localhost:8080/confirm?token='+token;
                     console.log(token);
-                    // send url in email
+                    // send confirmation email using emailjs
                     response.json("Email sent.");
                 });
             }
