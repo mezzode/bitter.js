e1740eb794592ec248319c19cd5cc2f7032157c2
Sean <toranos@gmail.com>
2016-02-23 18:13:44 +1100

    Add confirm page

--- public/app.js
+++ public/app.js
@@ -66,6 +66,50 @@ class Settings extends React.Component {
     }
 }
 
+class Confirm extends React.Component {
+    constructor() {
+        super();
+        this.state = {success: false}
+    }
+    componentDidMount() {
+        const token = this.props.location.query.token;
+        if (token) {
+            $.ajax({
+                url: '/api/user/',
+                method: 'POST',
+                dataType: 'json',
+                data: {token},
+                cache: false,
+                success: () => {
+                    this.setState({success: true});
+                },
+                error: (xhr, status, err) => {
+                    console.error(this.props.id, status, err.toString());
+                }
+            });
+        }
+    }
+    render() {
+        let message;
+        if (this.state.success) {
+            message = 'Success! You can now log in.';
+        } else {
+            message = 'Invalid URL';
+        }
+        return(
+            <div className="row">
+                <div className="col-md-3">
+                </div>
+                <div className="col-md-6 col-sm-12">
+                {message}
+                </div>
+                <div className="col-md-3">
+                </div>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
@@ -74,6 +118,7 @@ ReactDOM.render((
             <Route path="settings" component={Settings}/>
             <Route path="signup" component={Signup}/>
             <Route path="search/:term" component={Search}/>
+            <Route path="confirm" component={Confirm}/>
         </Route>
     </Router>
 ), document.getElementById('app'));
